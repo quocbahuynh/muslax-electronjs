f4ba698772c69d780a5068245bcecf3be6e57c28
quocbahuynh <jbrteamwebsite@gmail.com>
2021-09-02 16:16:41 +0700

    push musla app

--- main.js
+++ main.js
@@ -1,43 +1,96 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, Menu, Tray } = require("electron");
+const { platform } = require("os");
+const path = require("path");
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+var AutoLaunch = require("auto-launch");
+var autoLauncher = new AutoLaunch({
+  name: "Musla",
+});
+
+autoLauncher
+  .isEnabled()
+  .then(function (isEnabled) {
+    if (isEnabled) return;
+    autoLauncher.enable();
   })
+  .catch(function (err) {
+    throw err;
+  });
 
+let mainWindow, tray;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1027,
+    height: 650,
+    resizable: false,
+    fullscreen: false,
+    fullscreenable: false,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html");
 
+  let isAppQuitting = false;
+  app.on("before-quit", function (evt) {
+    isAppQuitting = true;
+  });
+
+  mainWindow.on("close", function (evt) {
+    if (!isAppQuitting) {
+      evt.preventDefault();
+      mainWindow.hide();
+      mainWindow.loadFile("index.html");
+    }
+  });
+ // mainWindow.setMenuBarVisibility(false);
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+  tray = new Tray("asstes/image/icon.png");
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Open",
+      click: function () {
+        mainWindow.show();
+      },
+    },
+    {
+      label: "Quit",
+      click: function () {
+        app.quit();
+      },
+    },
+  ]);
+  tray.setToolTip("Musla");
+  tray.setContextMenu(contextMenu);
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
 
-  app.on('activate', function () {
+// Set a variable when the app is quitting.
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -4,3 +4,108 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+
+document
+  .getElementsByClassName("apply")[0]
+  .addEventListener("click", selectSound);
+
+const checkBox = document.getElementsByName("sound");
+
+function keepSelect() {
+  const getValue = localStorage.getItem("xxx");
+  for (var i = 0; i < checkBox.length; i++) {
+    if (checkBox[i].value === getValue) {
+      checkBox[i].checked = true;
+    }
+  }
+}
+keepSelect();
+
+function selectOnlyThis(id) {
+  for (var i = 0; i < checkBox.length; i++) {
+    document.getElementById("Check" + i).checked = false;
+  }
+  document.getElementById(id).checked = true;
+}
+
+function play(sound) {
+  const audio = new Audio(`asstes/sounds/${sound}.mp3`);
+  audio.play();
+}
+
+function selectSound() {
+  for (let i = 0; i <= checkBox.length; i++) {
+    if (checkBox[i].checked === true) {
+      localStorage.clear();
+      localStorage.setItem("xxx", checkBox[i].value);
+      play(checkBox[i].value);
+    }
+  }
+}
+
+navigator.getBattery().then(function (battery) {
+  function updateAllBatteryInfo() {
+    updateChargeInfo();
+  }
+  updateAllBatteryInfo();
+
+  battery.addEventListener("chargingchange", function () {
+    updateChargeInfo();
+  });
+  function updateChargeInfo() {
+    if (battery.charging) {
+      const cat = localStorage.getItem("xxx");
+      play(cat);
+    }
+  }
+});
+
+//UI
+
+let flagPage = false;
+document.querySelector(
+  "body > div > div > div:nth-child(2) > div > div"
+).style.display = "none";
+
+// Home Page
+document
+  .querySelector(
+    "body > div > div > div.tile.is-4.is-vertical.is-parent > div > aside > ul:nth-child(2) > li:nth-child(1)"
+  )
+  .addEventListener("click", selectHome);
+
+function selectHome() {
+  if (flagPage === true) {
+    document.querySelector(
+      "body > div > div > div:nth-child(2) > div > div"
+    ).style.display = "none";
+
+    document.querySelector(
+      "body > div > div > div:nth-child(2) > div > nav"
+    ).style.display = "block";
+
+    flagPage = false;
+  }
+}
+
+// Document Page
+document
+  .querySelector(
+    "body > div > div > div.tile.is-4.is-vertical.is-parent > div > aside > ul:nth-child(4) > li:nth-child(1)"
+  )
+  .addEventListener("click", selectDocs);
+
+function selectDocs() {
+  if (flagPage === false) {
+    document.querySelector(
+      "body > div > div > div:nth-child(2) > div > div"
+    ).style.display = "block";
+
+    document.querySelector(
+      "body > div > div > div:nth-child(2) > div > nav"
+    ).style.display = "none";
+
+    flagPage = true;
+  }
+}
